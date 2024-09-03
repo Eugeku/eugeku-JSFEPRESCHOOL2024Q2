b45a5ca4b30a66d0068d5ad89ba094f175a7cef1
Eugeku <eugenekuzora@gmail.com>
2024-09-04 01:47:39 +0300

    feat: implement pagination

--- shelter/js/pets.js
+++ shelter/js/pets.js
@@ -1,7 +1,11 @@
+import { pagination } from './pagination/pagination.js';
+
 // Event listeners
 document.addEventListener("DOMContentLoaded", function () {
     // Links
     document.getElementById("header-logo-main").addEventListener("click", function () {
         document.location.href = './main.html';
     });
+
+    pagination();
 });
